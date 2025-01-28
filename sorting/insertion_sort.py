18c79fffae25c3eae4412f30f3bd0d60a86a8429
Nickson Kaigi <nickaigi@gmail.com>
2025-01-28 17:49:28 +0300

    insertion sort

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,18 @@
+"""
+Insertion Sort
+
+Insertion sort is a simple sorting algorithm that works by iteratively inserting each element of an unsorted list into its correct position in a sorted portion of the list. It is like sorting playing cards in your hands. You split the cards into two groups: the sorted cards and the unsorted cards. Then, you pick a card from the unsorted group and put it in the right place in the sorted group.
+
+We start with second element of the array as first element in the array is assumed to be sorted.
+Compare second element with the first element and check if the second element is smaller then swap them.
+Move to the third element and compare it with the first two elements and put at its correct position
+Repeat until the entire array is sorted.
+"""
+def insertion_sort(arr: list[int]) -> None:
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >=0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
