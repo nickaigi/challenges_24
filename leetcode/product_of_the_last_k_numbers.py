80eb8190b7e49f8e0d17b2b2d015de24add53e6d
Nickson Kaigi <nickaigi@gmail.com>
2025-03-12 21:48:21 +0300

    added the soln for the product of the last k numbers

--- leetcode/product_of_the_last_k_numbers.py
+++ leetcode/product_of_the_last_k_numbers.py
@@ -35,19 +35,33 @@ productOfNumbers.getProduct(2); // return 32. The product of the last 2 numbers 
 class ProductOfNumbers:
 
     def __init__(self):
-        self.numbers = []
-        
+        # Initialize the product list with 1 to handle multiplication logic
+        self.prefix_product = [1]
+        self.size = 0
 
     def add(self, num: int) -> None:
-        self.numbers.append(num)
-        
+        if num == 0:
+            # If num is 0, reset the cumulative products since multiplication
+            # with 0 invalidates previous products
+            self.prefix_product = [1]
+            self.size = 0
+        else:
+            # Append the cumulative product of the current number with the last
+            # product
+            self.prefix_product.append(
+                self.prefix_product[self.size] * num
+            )
+            self.size += 1
 
     def getProduct(self, k: int) -> int:
-        prod = 1
-        n = len(self.numbers) - k
-        for num in self.numbers[n:]:
-            prod *= num
-        return prod
+        # Check if the requested product length exceeds the size of the valid
+        # product list
+        if k > self.size:
+            return 0
+        # Compute the product of the last k elements using division
+        return (
+            self.prefix_product[self.size] // self.prefix_product[self.size - k]
+        )
 
 
 if __name__ == '__main__':
