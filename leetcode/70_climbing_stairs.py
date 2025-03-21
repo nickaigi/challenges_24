1cc203a9755ad1e6149350a6ea914f82640f4a87
Nickson Kaigi <nickaigi@gmail.com>
2025-03-21 20:37:53 +0300

    added an optimized soln

--- leetcode/70_climbing_stairs.py
+++ leetcode/70_climbing_stairs.py
@@ -24,6 +24,7 @@ Explanation: There are three ways to climb to the top.
 
 """
 class Solution:
+
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
@@ -33,3 +34,18 @@ class Solution:
         for i in range(2, n):
             a.append(a[i - 2] + a[i - 1])
         return a[-1]
+
+    def climbStairs_optimized(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        two_back = 1
+        one_back = 2
+
+        for i in range(2, n):
+            curr = two_back + one_back
+            two_back = one_back
+            one_back = curr
+        return curr
