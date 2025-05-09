18d133ad04bccce4269275a6dfa796e1091f98c1
Nickson Kaigi <nickaigi@gmail.com>
2025-05-09 20:11:10 +0300

    added the iterative approach for in-order traversal

---
+++ binary_trees/iterative_in_order_traversal.py
@@ -0,0 +1,27 @@
+"""
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+Use an iterative approach
+
+"""
+# Definition for a binary tree node.
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        result = []
+        current = root
+        while current or stack:
+            while current:
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            result.append(current.val)
+            current = current.right
+        return result
