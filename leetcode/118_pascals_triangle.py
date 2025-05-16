671e80f3d7e460108e41c152154b8dd237c4fdac
Nickson Kaigi <nickaigi@gmail.com>
2025-05-16 20:14:39 +0300

    added a modified version of generate

--- leetcode/118_pascals_triangle.py
+++ leetcode/118_pascals_triangle.py
@@ -2,7 +2,21 @@ from typing import List
 
 
 class Solution:
+
     def generate(self, numRows: int) -> List[List[int]]:
+        first_row = [1]
+        rows = [first_row]
+
+        for _ in range(1, numRows):
+            row = [1]
+            for i in range(1, len(rows[-1])):
+                num = rows[-1][i-1] + rows[-1][i]
+                row.append(num)
+            row.append(1)
+            rows.append(row)
+        return rows
+
+    def generate_nicks(self, numRows: int) -> List[List[int]]:
         res = [[1], [1, 1]]
 
         for i in range(2, numRows):
