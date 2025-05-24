a95f74e2b6a676cea1731fa94d00fa678283ed04
Nickson Kaigi <nickaigi@gmail.com>
2025-05-24 16:57:07 +0300

    added leetcode 88 sol

--- leetcode/88_merge_sorted_array.py
+++ leetcode/88_merge_sorted_array.py
@@ -36,12 +36,27 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1 = 0
-        p2 = 0
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
 
 if __name__ == '__main__':
     sol = Solution()
     nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    print(nums1)
     nums2 = [2, 5, 6]
+    n = 3
+    sol.merge(nums1, m, nums2, n)
+    print(nums1)
 
     assert nums1 == [1, 2, 2, 3, 5, 6]
