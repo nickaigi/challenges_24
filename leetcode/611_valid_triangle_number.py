5754aec2dff1b1e28b04e7a472044f074b32ddf3
Nickson Kaigi <nickaigi@gmail.com>
2025-09-26 19:46:03 +0300

    lc_611
    soln

--- leetcode/611_valid_triangle_number.py
+++ leetcode/611_valid_triangle_number.py
@@ -1,4 +1,22 @@
 class Solution:
     def triangleNumber(self, nums: list[int]) -> int:
-        res = 0
-        return res
+
+        def binary_search(nums: list[int], l: int, r: int, x: int) -> int:
+            while r >= l and r < len(nums):
+                mid = (l + r) // 2
+                if nums[mid] >= x:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return l
+
+        count = 0
+        nums.sort()
+        for i in range(0, len(nums) - 2):
+            k = i + 2
+            j = i + 1
+            while j < len(nums) - 1 and nums[i] != 0:
+                k = binary_search(nums, k, len(nums) - 1, nums[i] + nums[j])
+                count += k - j - 1
+                j += 1
+        return count
