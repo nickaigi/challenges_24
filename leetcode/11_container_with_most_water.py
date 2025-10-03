57e7c5f2fb9c6849e08f5f74e0a73711e17e7928
Nickson Kaigi <nickaigi@gmail.com>
2025-10-03 18:13:32 +0300

    update
    soln when give [0, 2]

--- leetcode/11_container_with_most_water.py
+++ leetcode/11_container_with_most_water.py
@@ -1,7 +1,6 @@
-
 class Solution:
     def maxArea(self, height: list[int]) -> int:
-        area = 1
+        area = 0
         n = len(height)
         for i in range(n):
             for j in range(i+1, n):
@@ -14,13 +13,18 @@ class Solution:
                     else:
                         h = height[i]
                         area = max(area, (h * w))
-                else:
+                elif height[j] == height[i] and (height[j] !=0 or height[i] !=0):
                     area = max(area, (height[j] * w))
+                else:
+                    area = max(area, 0)
         return area
+
 
 if __name__ == '__main__':
     sol = Solution()
     height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
     print(sol.maxArea(height))
     height = [1, 1]
+    print(sol.maxArea(height))
+    height = [0, 2]
     print(sol.maxArea(height))
