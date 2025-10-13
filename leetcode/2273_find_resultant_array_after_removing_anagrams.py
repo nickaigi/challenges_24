a32db2b4635c67adb3436d48d800f71b36583340
Nickson Kaigi <nickaigi@gmail.com>
2025-10-13 15:45:36 +0300

    soln
    added the solution to lc 2273 by using a frequency array

--- leetcode/2273_find_resultant_array_after_removing_anagrams.py
+++ leetcode/2273_find_resultant_array_after_removing_anagrams.py
@@ -1,10 +1,22 @@
 class Solution:
     def removeAnagrams(self, words: list[str]) -> list[str]:
-        res = set()
-        for w in words:
-            if ''.join(sorted(w)) not in res:
-                res.add(w)
-        return list(res)
+        res = [words[0]]
+        n = len(words)
+
+        def compare(word1: str, word2: str) -> bool:
+            freq = [0] * 26
+            for ch in word1:
+                freq[ord(ch) - ord('a')] += 1
+
+            for ch in word2:
+                freq[ord(ch) - ord('a')] -= 1
+            return all(x == 0 for x in freq)
+
+        for i in range(1, n):
+            if compare(words[i], words[i-1]):
+                continue
+            res.append(words[i])
+        return res
 
 
 if __name__ == '__main__':
