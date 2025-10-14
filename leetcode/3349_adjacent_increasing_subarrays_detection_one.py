a71e3e42bed24a0dbc0b29176a75214b2f6004a5
Nickson Kaigi <nickaigi@gmail.com>
2025-10-14 14:57:00 +0300

    soln
    added the soln

--- leetcode/3349_adjacent_increasing_subarrays_detection_one.py
+++ leetcode/3349_adjacent_increasing_subarrays_detection_one.py
@@ -1,6 +1,15 @@
 class Solution:
     def hasIncreasingSubarrays(self, nums: list[int], k: int) -> bool:
-        return False
+        n = len(nums)
+        current, prev_current, ans = 1, 0, 0
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                current += 1
+            else:
+                prev_current, current = current, 1
+            ans = max(ans, min(prev_current, current))
+            ans = max(ans, current // 2)
+        return ans >= k
 
 
 if __name__ == '__main__':
