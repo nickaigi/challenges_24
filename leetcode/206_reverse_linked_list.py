b17107cd716ea25ea32d7897bf2590f0bbfab640
Nickson Kaigi <nickaigi@gmail.com>
2026-01-06 17:13:26 +0300

    changed lc206

--- leetcode/206_reverse_linked_list.py
+++ leetcode/206_reverse_linked_list.py
@@ -9,11 +9,12 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node = None
+        prev = None
+        curr = head
 
-        while head:
-            temp = head.next
-            head.next = node
-            node = head
-            head = temp
-        return node
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
