a6c45e2f41581f10a75caba1198f9e95ebb5d1a5
Nickson Kaigi <nickaigi@gmail.com>
2026-07-15 19:10:51 +0300

    lc 1877
    added soln

---
+++ leetcode/1877_minimize_max_pair_sum_in_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minPairSum(self, nums: list[int]) -> int:
+        nums.sort()
+        max_sum = 0
+        for i in range(len(nums)):
+            max_sum = max(max_sum, nums[i] + nums[len(nums) - 1 - i])
+        return max_sum
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [3, 5, 2, 3]
+    assert sol.minPairSum(nums) == 7
+    nums = [3, 5, 4, 2, 4, 6]
+    assert sol.minPairSum(nums) == 8
