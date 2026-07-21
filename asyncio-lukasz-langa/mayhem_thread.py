4de652c10cd23c2181046d545ed3ebf50b2158b0
Nickson Kaigi <nickaigi@gmail.com>
2026-07-21 19:49:24 +0300

    asyncio
    watching the intro to asyncio
    
    working on htmx examples

---
+++ asyncio-lukasz-langa/mayhem_thread.py
@@ -0,0 +1,20 @@
+import time
+import threading
+from typing import Mapping
+
+
+class Mayhem(threading.Thread):
+    def __init__(self, map: Mapping[str, int]) -> None:
+        super().__init__()
+        self.map = map
+
+    def run(self) -> None:
+        for key, value in self.map.items():
+            time.sleep(value)
+
+
+if __name__ == "__main__":
+    d = {"k1": 1, "k2": 2, "k3": 3}
+    m = Mayhem(d)
+    m.start()
+    d["k4"] = 4  # RuntimeError: dictionary changed size during iteration
