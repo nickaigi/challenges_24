e6e2f2a6b2f89609b52e13de50ba0d4db4db10ef
Nickson Kaigi <nickaigi@gmail.com>
2026-07-22 20:04:33 +0300

    lsbtw
    working version of ls with error checking and ignoring dotfiles

--- lsbtw/ls.c
+++ lsbtw/ls.c
@@ -1,6 +1,20 @@
+#include <dirent.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("%s\n", argv[0]);
+  char *path = (argc > 1) ? argv[1] : ".";
+
+  DIR *dir = opendir(path); // DIR is called an opaque pointer
+  if (!dir) {
+    perror("opendir");
+    return 1;
+  }
+  struct dirent *entry;
+  while ((entry = readdir(dir)) != NULL) {
+    if (entry->d_name[0] == '.')
+      continue;
+    printf("%s\n", entry->d_name);
+  }
+  closedir(dir);
   return 0;
 }
