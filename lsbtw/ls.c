abca9463d7ae3ed9da71db182ecaf3cd431aad8a
Nickson Kaigi <nickaigi@gmail.com>
2026-07-23 20:43:42 +0300

    working of -l
    have created the permission string

--- lsbtw/ls.c
+++ lsbtw/ls.c
@@ -1,7 +1,35 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
+void mode_string(mode_t mode, char *str) {
+  if (S_ISDIR(mode))
+    str[0] = 'd';
+  else if (S_ISLNK(mode))
+    str[0] = 'l';
+  else if (S_ISCHR(mode))
+    str[0] = 'c';
+  else if (S_ISBLK(mode))
+    str[0] = 'b';
+  else if (S_ISFIFO(mode))
+    str[0] = 'p';
+  else if (S_ISSOCK(mode))
+    str[0] = 's';
+  else
+    str[0] = '-';
+
+  str[1] = (mode & S_IRUSR) ? 'r' : '-';
+  str[2] = (mode & S_IWUSR) ? 'w' : '-';
+  str[3] = (mode & S_IXUSR) ? 'x' : '-';
+  str[4] = (mode & S_IRGRP) ? 'r' : '-';
+  str[5] = (mode & S_IWGRP) ? 'w' : '-';
+  str[6] = (mode & S_IXGRP) ? 'x' : '-';
+  str[7] = (mode & S_IROTH) ? 'r' : '-';
+  str[8] = (mode & S_IWOTH) ? 'w' : '-';
+  str[9] = (mode & S_IXOTH) ? 'x' : '-';
+  str[10] = '\0';
+}
 int show_all = 0;
 
 int main(int argc, char *argv[]) {
