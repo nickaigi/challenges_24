62713d283a0f0e476731c849f01330e96246431c
Nickson Kaigi <nickaigi@gmail.com>
2026-08-10 15:40:04 +0300

    added dijkstra one

--- dijkstras-algorithm/dijkstras.py
+++ dijkstras-algorithm/dijkstras.py
@@ -1,58 +1,63 @@
-def dijkstras_algorithm():
+import heapq
 
-    def find_lowest_cost_node(costs):
-        lowest_cost = float("inf")
-        lowest_cost_node = None
-        for node in costs:
-            cost = costs[node]
-            if cost < lowest_cost and node not in processed:
-                lowest_cost = cost
-                lowest_cost_node = node
-        return lowest_cost_node
-
-    graph, costs, parents = set_up()
-    processed = set()
-
-    node = find_lowest_cost_node(costs)
-    while node:
-        cost = costs[node]
-        neighbors = graph[node]
-        for n in neighbors.keys():
-            new_cost = cost + neighbors[n]
-            if costs[n] > new_cost:
-                costs[n] = new_cost
-        processed.add(node)
-        node = find_lowest_cost_node(costs)
+GraphDict = dict[str, dict[str, int]]
+Distances = dict[str, int | float]
 
 
-def set_up() -> tuple:
-    graph = {}
-    costs = {}
-    parents = {}
+def get_shortest_path(previous_nodes, destination):
+    path = []
+    current_node = destination
+    while current_node is not None:
+        path.append(current_node)
+        current_node = previous_nodes[current_node]
 
-    graph["start"] = {}
-    graph["start"]["a"] = 6
-    graph["start"]["b"] = 2
+    path.reverse()
+    return path
 
-    graph["a"] = {}
-    graph["a"]["fin"] = 1
 
-    graph["b"] = {}
-    graph["b"]["a"] = 3
-    graph["b"]["fin"] = 5
+def dijkstra(graph: GraphDict, start_node: str) -> tuple:
+    distances_from_source: Distances = {node: float("inf") for node in graph}
+    distances_from_source[start_node] = 0
+    previous_nodes = {node: None for node in graph}
+    visited = set()
 
-    graph["fin"] = {}
+    to_visit = [(0, start_node)]  # min-heap
+    while to_visit:
+        current_distance, current_node = heapq.heappop(to_visit)
+        if current_node in visited:
+            continue
 
-    costs["a"] = 6
-    costs["b"] = 2
-    costs["fin"] = float("inf")
+        for neighbor, weight in graph[current_node].items():
+            if neighbor in visited:
+                continue
 
-    parents["a"] = "start"
-    parents["b"] = "start"
-    parents["fin"] = None
+            distance = current_distance + weight
+            if distance < distances_from_source[neighbor]:
+                distances_from_source[neighbor] = distance
+                previous_nodes[neighbor] = current_node
+                heapq.heappush(to_visit, (distance, neighbor))
+            visited.add(current_node)
 
-    return graph, costs, parents
+    return distances_from_source, previous_nodes
+
+
+def main() -> None:
+    graph: GraphDict = {
+        "A": {"B": 2, "C": 6},
+        "B": {"A": 2, "C": 9, "D": 5},
+        "C": {"A": 6, "B": 9, "D": 8},
+        "D": {"B": 5, "C": 8},
+    }
+    # Expected:
+    # (
+    #   {'A': 0, 'B': 2, 'C': 6, 'D', 7}
+    #   {'A': None, 'B': 'A', 'C': 'A', 'D': 'B'}
+    # )
+    assert (
+        {"A": 0, "B": 2, "C": 6, "D": 7},
+        {"A": None, "B": "A", "C": "A", "D": "B"},
+    ) == dijkstra(graph, "A")
 
 
 if __name__ == "__main__":
-    graph, costs, parents = set_up()
+    main()
