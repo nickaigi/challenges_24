e3aaae28a3f59a6067b9fa5f39ce6c6063507ef4
Nickson Kaigi <nickaigi@gmail.com>
2026-08-27 16:59:00 +0300

    lc 560
    soln

--- leetcode/560_subarray_sum_equals_k.py
+++ leetcode/560_subarray_sum_equals_k.py
@@ -1,6 +1,15 @@
 class Solution:
     def subarraysum(self, nums: list[int], k: int) -> int:
         count = 0
+        curr_sum = 0
+        prefix_sums = {0: 1}
+
+        for num in nums:
+            curr_sum += num
+            diff = curr_sum - k
+
+            count += prefix_sums.get(diff, 0)
+            prefix_sums[curr_sum] = 1 + prefix_sums.get(curr_sum, 0)
         return count
 
     def subarraysum_brute_force(self, nums: list[int], k: int) -> int:
