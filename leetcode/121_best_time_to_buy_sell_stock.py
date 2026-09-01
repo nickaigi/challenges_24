a63ccf147f51b1ad80c5186658d2ee3a709aeee4
Nickson Kaigi <nickaigi@gmail.com>
2026-09-01 18:09:10 +0300

    lc 121
    adjusted to add a brute force approach

--- leetcode/121_best_time_to_buy_sell_stock.py
+++ leetcode/121_best_time_to_buy_sell_stock.py
@@ -1,13 +1,29 @@
-from typing import List
-
-
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        price_to_buy = float("inf")
-        max_profit = 0.0
+    def maxProfit_bruteforce(self, prices: list[int]) -> int:
+        """
+        Time: O(n^2)
+        Space: O(1)
+        """
+        max_profit = float("-inf")
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                profit = prices[j] - prices[i]
+
+                if profit > 0:
+                    max_profit = max(max_profit, profit)
+        return int(max_profit if max_profit > float("-inf") else 0)
+
+    def maxProfit(self, prices: list[int]) -> int:
+        min_price = float("inf")
+        max_profit = 0
+
         for price in prices:
-            if price < price_to_buy:
-                price_to_buy = price
-            else:
-                max_profit = max(max_profit, price - price_to_buy)
-        return int(max_profit)
+            if price < min_price:
+                min_price = price
+
+            profit = price - min_price
+
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
---
+++ leetcode/2058_find_min_and_max_nodes_btwn_critical_points.py
@@ -0,0 +1,9 @@
+class ListNode:
+    def __init__(self, val=0, next=None) -> None:
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: ListNode | None) -> list[int]:
+        pass
